fa3e77b71982a430016e8efcb0a960cb591091fb
vijay11234 <70164574+vijay11234@users.noreply.github.com>
2020-12-15 10:11:17 +0530

    Added: Observer design pattern program

---
+++ Observer Pattern/ObserverPattern.js
@@ -0,0 +1,35 @@
+function Subject() {
+    this.observers = []
+}
+
+Subject.prototype = {
+    subscribe: function(fn) {
+        this.observers.push(fn)
+    },
+    unsubscribe: function (fnToRemove) {
+        this.observers = this.observers.filter(fn => {
+            if(fn != fnToRemove)
+                return fn
+        })
+    },
+    fire: function () {
+        this.observers.forEach(fn => {
+            fn.call()
+        })
+    }
+}
+
+const subject = new Subject()
+
+function Observer1() {
+    console.log("Observer1 firing")
+}
+
+function Observer2() {
+    console.log("Observer2 firing")
+}
+
+subject.subscribe(Observer1)
+subject.subscribe(Observer2)
+
+subject.fire()
